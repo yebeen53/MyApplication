b68ecf265c3eef559f7491938ef06dccdf6e827f
yebeen53 <yebeenlee0503@naver.com>
2025-01-14 15:13:10 +0900

    Update color scheme

--- app/src/main/java/com/example/myapplication/ui/theme/Color.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Color.kt
@@ -9,3 +9,12 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Black = Color(0xFF000000)
+val White =Color(0xFFFFFFFF)
+val Rose =Color(0xFFE8C1BC)
+val PurpleS=Color(0xFFDECFE2)
+val Blue =Color(0xFFA4C8D6)
+val Mint =Color(0xFFA6EFDC)
+
+
